0acbd2c278eb9762d7d0480d0286b10d92745168
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-06-30 15:58:36 +0300

    test

---
+++ js/1-2.js
@@ -0,0 +1,55 @@
+// const stars = 3;
+// let price;
+
+// if (stars === 1) {
+//     price = 20;
+// } else if (stars === 2) {
+//     price = 30;
+// } else {
+//     console.log('there is no such stars');
+// }
+
+// switch (stars) {
+//     case 1:
+//         price = 20;
+//         break;
+//     case 2:
+//         price = 30
+//         break;
+//     default:
+//         console.log('there is no such stars');
+// }
+
+// console.log(price);
+
+// const stars = 5;
+// let price;
+
+// if (stars === 1 || stars === 2) {
+//     price = 20;
+// } else if (stars === 3 || stars === 4) {
+//     price = 30;
+// } else if (stars === 5) {
+//     price = 120;
+// } else {
+//     console.log('there is no such stars');
+// }
+
+// console.log(price);
+
+// switch (stars) {
+//     case 1:
+//     case 2:
+//         price = 20;
+//         break;
+//     case 3:
+//     case 4:
+//         price = 30
+//         break;
+//     default:
+//         console.log('there is no such stars');
+// }
+
+// console.log(price);
+
+const option = 1;
