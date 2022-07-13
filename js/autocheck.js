b5d1baf1f9746c410a32e3314b24e9a1e3112432
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-07-13 16:04:52 +0300

    study

---
+++ js/autocheck.js
@@ -0,0 +1,15 @@
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+  // Change code below this line
+  salaries = {
+    name: salary,
+  }
+  const value = Object.values(salaries);
+
+  for (let value of values){
+    totalSalary += value;
+  }
+
+  // Change code above this line
+  return totalSalary;
+}
