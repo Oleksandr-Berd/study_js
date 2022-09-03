03abc610896667579e3d96be010b30da2ba02260
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-09-03 12:23:38 +0300

    ss

--- js/test.js
+++ js/test.js
@@ -1,5 +1,7 @@
-function Dog() {
-  this.name: "Infinity",
-    this.color: "Yellow-blue",
-    this.numLegs: 4,
+function destroyer(arr) {
+  return arr;
 }
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
