e71a82838684dac20f0317a32b0dd0877b7b2e15
Oleksandr <105509345+Oleksandr-Berd@users.noreply.github.com>
2022-10-24 12:26:04 +0300

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,13 +1,14 @@
-function getRealFloor(n) {
-  let euroFloor = 0;
+function howManyGifts(maxBudget, gifts) {
+  let total = 0;
+  let ableGifts = [];
+  ableGifts = gifts.map((gift) => {
+    total = total + gift;
+    if (total < maxBudget) {
+      return total;
+    }
+  }, 0);
 
-  if (n < 13 && n > 0) {
-    return (euroFloor = n - 1);
-  } else if (n <= 0) {
-    return euroFloor === n;
-  } else {
-    return (euroFloor = n - 2);
-  }
+  return ableGifts;
 }
 
-console.log(getRealFloor(0));
+console.log(howManyGifts(20, [13, 2, 4, 6, 1]));
